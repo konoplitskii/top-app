f70ef0fa4221a6b7b4ffa34a424a687b9767a877
Sergey <97333505+konoplitskii@users.noreply.github.com>
2022-08-14 19:46:34 +0300

    add component Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import {Htag, P} from "../components";
 import Button from "../components/Button/Button";
+import {Tag} from "../components/Tag/Tag";
 
 export default function Home() {
   return (
@@ -14,6 +15,10 @@ export default function Home() {
 			<P size="l">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequatur consequuntur culpa, distinctio fugit impedit minima molestiae nostrum praesentium recusandae reiciendis reprehenderit rerum saepe velit voluptatum? Debitis doloremque impedit soluta.
 			</P>
+			<Tag color="primary" size="m">sds</Tag>
+			<Tag color="red" size="m">sds</Tag>
+			<Tag color="green" size="s">sds</Tag>
+			<Tag color="grey" size="m">sds</Tag>
     </div>
   )
 }
